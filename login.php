bd1889f45d42cf5bb6b93baf918918bdc8225ea5
Govind Bavkar <tectignisofficial@gmail.com>
2022-11-04 19:56:28 +0530

    UPDATE

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta http-equiv="x-ua-compatible" content="ie=edge">
-<title>Account &ndash; Belle Multipurpose Bootstrap 4 Template</title>
+<title>Account &ndash; Z1</title>
 <meta name="description" content="description">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- Favicon -->
