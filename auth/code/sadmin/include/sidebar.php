1643a4f287d9030fb97aa1549506d9d9a6aa93c4
whynaidu <naiduvedant@gmail.com>
2022-11-04 10:37:18 +0530

    sidebar

--- auth/code/sadmin/include/sidebar.php
+++ auth/code/sadmin/include/sidebar.php
@@ -35,7 +35,7 @@
             <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
                 <li class=" nav-item"><a class="d-flex align-items-center" href="index.php"><i data-feather="home"></i><span class="menu-title text-truncate" data-i18n="Dashboards">Dashboards</span><span class="badge badge-light-warning rounded-pill ms-auto me-1">2</span></a>
                 </li>
-                <li class=" navigation-header"><span data-i18n="Apps &amp; Pages">Apps &amp; Pages</span><i data-feather="more-horizontal"></i>
+                <li class=" nav-item"><a class="d-flex align-items-center" href="pages.php"><i data-feather="mail"></i><span class="menu-title text-truncate" data-i18n="Email">Pages</span></a>
                 </li>
                                 <li class=" nav-item"><a class="d-flex align-items-center" href="#"><i data-feather="file-text"></i><span class="menu-title text-truncate" data-i18n="Invoice">Blogs</span></a>
                     <ul class="menu-content">
