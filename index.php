14aef9811a58c3f4cc9595516ce1d99359ae12a0
Govind Bavkar <tectignisofficial@gmail.com>
2022-11-05 14:07:03 +0530

    update

--- index.php
+++ index.php
@@ -55,27 +55,14 @@
         	<div class="home-slideshow">
             	<div class="slide">
                 	<div class="blur-up lazyload">
-                        <img class="blur-up lazyload" data-src="assets/images/slideshow-banners/z1kneebracebanner1.jpg" src="assets/images/slideshow-banners/z1kneebracebanner1.jpg" alt="Z1 KNEE BRACES" title="Z1 KNEE BRACES" />
-                        <div class="slideshow__text-wrap slideshow__overlay classic middle">
-                            <div class="slideshow__text-content middle">
-                            </div>
-                        </div>
+                        <img class="blur-up lazyload" data-src="assets/images/slideshow-banners/z1kneebracebanner1.jpg" src="assets/images/slideshow-banners/z1kneebracebanner1.jpg" alt="z1 knee braces" title="z1 knee braces" />
+                     
                     </div>
                 </div>
                 <div class="slide">
                 	<div class="blur-up lazyload">
-                        <img class="blur-up lazyload" data-src="assets/images/slideshow-banners/home2-default-banner2.jpg" src="assets/images/slideshow-banners/home2-default-banner2.jpg" alt="Summer Bikini Collection" title="Summer Bikini Collection" />
-                        <div class="slideshow__text-wrap slideshow__overlay classic middle">
-                            <div class="slideshow__text-content middle">
-                            	<div class="container">
-                                    <div class="wrap-caption center">
-                                        <h2 class="h1 mega-title slideshow__title">Fashion &amp; Show</h2>
-                                        <span class="mega-subtitle slideshow__subtitle">A World Fashion and Trendy Fashion Clother's</span>
-                                        <span class="btn">Shop now</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        <img class="blur-up lazyload" data-src="assets/images/slideshow-banners/z1kneebracebanner2" src="assets/images/slideshow-banners/z1kneebracebanner2" alt="z1 knee braces" title="z1 knee braces" />
+                     
                     </div>
                 </div>
                 <div class="slide">
