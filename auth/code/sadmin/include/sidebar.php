a2b4ab25c2c7d961cc1b520000e7eaac0dacbe74
Govind Bavkar <tectignisofficial@gmail.com>
2022-11-08 10:57:30 +0530

    update

--- auth/code/sadmin/include/sidebar.php
+++ auth/code/sadmin/include/sidebar.php
@@ -77,8 +77,7 @@
                 </li>
                 <li class=" nav-item"><a class="d-flex align-items-center" href="#"><i data-feather="shield"></i><span class="menu-title text-truncate" data-i18n="Roles &amp; Permission">Payments</span></a>
                     <ul class="menu-content">
-                        <li><a class="d-flex align-items-center" href="transactions.php"><i data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Roles">Transactions</span></a>
-                        </li>
+
                         <li><a class="d-flex align-items-center" href="paymentsmethods.php"><i data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Permission">Payment Methods</span></a>
                         </li>
                     </ul>
