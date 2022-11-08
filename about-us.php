be5066fafe79a94e5b5cb4fb8fdcbc461abda43e
whynaidu <naiduvedant@gmail.com>
2022-11-08 13:33:31 +0530

    about us

--- about-us.php
+++ about-us.php
@@ -2,107 +2,146 @@
 <html class="no-js" lang="en">
 
 <!-- belle/about-us.html   11 Nov 2019 12:44:33 GMT -->
+
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta http-equiv="x-ua-compatible" content="ie=edge">
-<title>About Us &ndash; Belle Multipurpose Bootstrap 4 Template</title>
-<meta name="description" content="description">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<!-- Favicon -->
-<link rel="shortcut icon" href="assets/images/favicon.png" />
-<!-- Plugins CSS -->
-<link rel="stylesheet" href="assets/css/plugins.css">
-<!-- Bootstap CSS -->
-<link rel="stylesheet" href="assets/css/bootstrap.min.css">
-<!-- Main Style CSS -->
-<link rel="stylesheet" href="assets/css/style.css">
-<link rel="stylesheet" href="assets/css/responsive.css">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>About Us &ndash; Belle Multipurpose Bootstrap 4 Template</title>
+    <meta name="description" content="description">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="assets/images/favicon.png" />
+    <!-- Plugins CSS -->
+    <link rel="stylesheet" href="assets/css/plugins.css">
+    <!-- Bootstap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <!-- Main Style CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
 </head>
+
 <body class="page-template belle">
-<div class="pageWrapper">
-	<!--Search Form Drawer-->
-  <?php include("include/header.php");?>
-	<!--End Mobile Menu-->
-    
-    <!--Body Content-->
-    <div id="page-content">
-    	<!--Page Title-->
-    	<div class="page section-header text-center">
-			<div class="page-title">
-        		<div class="wrapper"><h1 class="page-width">About Us</h1></div>
-      		</div>
-		</div>
-        <!--End Page Title-->
-        
-        <div class="container">
-        	<div class="row">
-                <div class="col-12 col-sm-12 col-md-12 col-lg-12 main-col">
-                	<div class="text-center mb-4">
-                        <h2 class="h2"><b>Our Story</b></h2>
-                        <div class="rte-setting" style="font-size:15px">
-                            <p>Zodiacal Overseas Pvt. Ltd. has pioneered the healthcare industry since it’s inception in 2012 as a professional manufacturing and marketing company dealing in various healthcare products based in India. With unique product and flawless marketing strategies, ZOPL has been a global market leader in the course of a very short span of time. Z1, Z-ONE & Z1 KNEEBRACE all our brands.</p>
+    <div class="pageWrapper">
+        <!--Search Form Drawer-->
+        <?php include("include/header.php");?>
+        <!--End Mobile Menu-->
+
+        <!--Body Content-->
+        <div id="page-content">
+            <!--Page Title-->
+            <div class="page section-header text-center">
+                <div class="page-title">
+                    <div class="wrapper">
+                        <h1 class="page-width">About Us</h1>
+                    </div>
+                </div>
+            </div>
+            <!--End Page Title-->
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-sm-12 col-md-12 col-lg-12 main-col">
+                        <div class="text-center mb-4">
+                            <h2 class="h2"><b>Our Story</b></h2>
+                            <div class="rte-setting" style="font-size:15px">
+                                <p>Zodiacal Overseas Pvt. Ltd. has pioneered the healthcare industry since it’s
+                                    inception in 2012 as a professional manufacturing and marketing company dealing in
+                                    various healthcare products based in India. With unique product and flawless
+                                    marketing strategies, ZOPL has been a global market leader in the course of a very
+                                    short span of time. Z1, Z-ONE & Z1 KNEEBRACE all our brands.</p>
+                            </div>
                         </div>
                     </div>
-               	</div>
-            </div>
+                </div>
 
-            <div class="section feature-content">
-                <div class="container">
-                    <div class="row">
-                        <div class="feature-row">
-                            <div class="col-12 col-sm-12 col-md-6 feature-row__item">
-                                <img src="assets/images/fast-fashion_600x.jpg" alt="Fast Fashion Only available at BElle" title="Fast Fashion Only available at BElle">
-                            </div>
-                            <div class="col-12 col-sm-12 col-md-6 feature-row__item feature-row__text feature-row__text--left text-left">
-                                <div class="row-text">
-                                    <h2 class="h1" style="font-size:15px"><b>Our USP</b></h2>
-                                    <div class="rte-setting featured-row__subtext" style="font-size:15px">
-                                        <p>We at ZOPL ( Zodiacal Overseas Pvt. Ltd. ) make sure each and every product we manufacture and sell are based on the needs and convenience of our customers. We boast of manufacturing some of the most unquestionably superior quality healthcare products with utmost healing touch like knee braces, ankle braces & various varieties of supports not only in India but globally which sets us apart and makes us what we are today. You can now get the products right here at z1kneebrace.com.</p>
+                <div class="section feature-content">
+                    <div class="container">
+                        <div class="row">
+                            <div class="feature-row">
+                                <div class="col-12 col-sm-12 col-md-6 feature-row__item">
+                                    <img src="assets/images/fast-fashion_600x.jpg"
+                                        alt="Fast Fashion Only available at BElle"
+                                        title="Fast Fashion Only available at BElle">
+                                </div>
+                                <div
+                                    class="col-12 col-sm-12 col-md-6 feature-row__item feature-row__text feature-row__text--left text-left">
+                                    <div class="row-text">
+                                        <h2 class="h1" style="font-size:15px"><b>Our USP</b></h2>
+                                        <div class="rte-setting featured-row__subtext" style="font-size:15px">
+                                            <p>We at ZOPL ( Zodiacal Overseas Pvt. Ltd. ) make sure each and every
+                                                product we manufacture and sell are based on the needs and convenience
+                                                of our customers. We boast of manufacturing some of the most
+                                                unquestionably superior quality healthcare products with utmost healing
+                                                touch like knee braces, ankle braces & various varieties of supports not
+                                                only in India but globally which sets us apart and makes us what we are
+                                                today. You can now get the products right here at z1kneebrace.com.</p>
+                                        </div>
+
                                     </div>
-                                  
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+                <div class="section feature-content">
+                    <div class="container">
+                        <div class="row">
+                            <div class="feature-row">
+                                <div
+                                    class="col-12 col-sm-12 col-md-6 feature-row__item feature-row__text feature-row__text--right text-left">
+                                    <div class="row-text" style="margin-left:0px">
+                                        <h2 class="h1"><b>WE ARE CERTIFIED BY</b></h2>
+                                        <div class="rte-setting featured-row__subtext" style="font-size:15px">
+                                            <p>We are certified by ISO 9001:2015. All the products manufactured at
+                                                Zodiacal Overseas Pvt. Ltd. hold the CE marking on all our manufactured
+                                                products which ensures docility of safety, wellness, as well as
+                                                environmentally friendly models for all the products, made and sold in
+                                                European regions. We are also certified by FDA, The Food and Drug
+                                                Administration of the United States Department of Health and Human
+                                                Services. </p>
+                                            <p>Our braces and various other healthcare products are of the leading quality. We at Z1 guarantee on taking into consideration the needs of
+                                                our customers and meeting them, providing them complete satisfaction by
+                                                regular checks and up-gradation in the various processes involved.</p>
+                                        </div>
+
+                                    </div>
+                                </div>
+                                <div class="col-12 col-sm-12 col-md-6 feature-row__item">
+                                    <img src="assets/images/aboutus.jpg" alt="K4 OsteoAlign Knee Orthosis"
+                                        title="K4 OsteoAlign Knee Orthosis">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
 
-            <div class="row">
-            	<div class="col-6" style="font-size:15px">
-                    <h2 >WE ARE CERTIFIED BY</h2>
-                    <p>We are certified by ISO 9001:2015. All the products manufactured at Zodiacal Overseas Pvt. Ltd. hold the CE marking on all our manufactured products which ensures docility of safety, wellness, as well as environmentally friendly models for all the products, made and sold in European regions. We are also certified by FDA, The Food and Drug Administration of the United States Department of Health and Human Services. </p>
-                    <p>Our braces and various other healthcare products are of the leading quality. We at Z1 guarantee on taking into consideration the needs of our customers and meeting them, providing them complete satisfaction by regular checks and up-gradation in the various processes involved.</p>
-                    <p></p>
-                </div>
-                <div class="col-6">
-                <img src="assets/images/aboutus.jpg" alt="" />
-                </div>
-            </div>  
         </div>
-        
+        <!--End Body Content-->
+
+        <!--Footer-->
+        <?php include("include/footer.php");?>
+        <!--End Footer-->
+        <!--Scoll Top-->
+        <span id="site-scroll"><i class="icon anm anm-angle-up-r"></i></span>
+        <!--End Scoll Top-->
+
+        <!-- Including Jquery -->
+        <script src="assets/js/vendor/jquery-3.3.1.min.js"></script>
+        <script src="assets/js/vendor/jquery.cookie.js"></script>
+        <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
+        <script src="assets/js/vendor/wow.min.js"></script>
+        <!-- Including Javascript -->
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/plugins.js"></script>
+        <script src="assets/js/popper.min.js"></script>
+        <script src="assets/js/lazysizes.js"></script>
+        <script src="assets/js/main.js"></script>
     </div>
-    <!--End Body Content-->
-    
-    <!--Footer-->
-           <?php include("include/footer.php");?>
-    <!--End Footer-->
-    <!--Scoll Top-->
-    <span id="site-scroll"><i class="icon anm anm-angle-up-r"></i></span>
-    <!--End Scoll Top-->
-    
-     <!-- Including Jquery -->
-     <script src="assets/js/vendor/jquery-3.3.1.min.js"></script>
-     <script src="assets/js/vendor/jquery.cookie.js"></script>
-     <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
-     <script src="assets/js/vendor/wow.min.js"></script>
-     <!-- Including Javascript -->
-     <script src="assets/js/bootstrap.min.js"></script>
-     <script src="assets/js/plugins.js"></script>
-     <script src="assets/js/popper.min.js"></script>
-     <script src="assets/js/lazysizes.js"></script>
-     <script src="assets/js/main.js"></script>
-</div>
 </body>
 
 <!-- belle/about-us.html   11 Nov 2019 12:44:39 GMT -->
+
 </html>
