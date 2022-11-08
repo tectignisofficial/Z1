93b79c38963ba3e06461a6cc0c216554c7c52414
Govind Bavkar <tectignisofficial@gmail.com>
2022-11-08 08:56:34 +0530

    update

--- index.php
+++ index.php
@@ -85,8 +85,16 @@
         	<div class="home-slideshow">
             	<div class="slide">
                 	<div class="blur-up lazyload bg-size">
-                        <img class="blur-up lazyload bg-img" data-src="assets/images/slideshow-banners/K2-COMFORTLINE-KNEE-ORTHOSIS1.jpg" src="assets/images/slideshow-banners/K2-COMFORTLINE-KNEE-ORTHOSIS1.jpg" alt="K2 COMFORTLINE KNEE ORTHOSIS1" title="K2 COMFORTLINE KNEE ORTHOSIS1" />
-
+                        <img class="blur-up lazyload bg-img" data-src="assets/images/slideshow-banners/K2-COMFORTLINE-KNEE-ORTHOSIS01.jpg" src="assets/images/slideshow-banners/K2-COMFORTLINE-KNEE-ORTHOSIS01.jpg" alt="K2 COMFORTLINE KNEE ORTHOSIS1" title="K2 COMFORTLINE KNEE ORTHOSIS1" />
+                        <div class="slideshow__text-wrap slideshow__overlay classic bottom">
+                            <div class="slideshow__text-content bottom">
+                                <div class="wrap-caption center">
+                                        <h2 class="h1 mega-title slideshow__title">K2 COMFORTLINE KNEE ORTHOSIS1</h2>
+                                        <span class="mega-subtitle slideshow__subtitle">From Hight to low, classic or modern. We have you covered</span>
+                                        <a href="shop.php"><span class="btn">Shop now</span></a>
+                                    </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="slide">
