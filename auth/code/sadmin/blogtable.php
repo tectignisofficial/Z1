27b3b8d04e21b380fdbaa7e6964ff7728c050438
monikagore <mobnikgore5=6@m=gmail.com>
2022-11-09 11:35:44 +0530

    ch

--- auth/code/sadmin/blogtable.php
+++ auth/code/sadmin/blogtable.php
@@ -233,11 +233,6 @@ include("../../../include/config.php");
     <script src="app-assets/vendors/js/pickers/flatpickr/flatpickr.min.js"></script>
 
 
-
-
-
-
-
     <script src="plugins/datatables/jquery.dataTables.min.js"></script>
     <script src="plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
     <script src="plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
