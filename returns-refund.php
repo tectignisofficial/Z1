0ff4fd7599af0fead2c158f1a31867ccedfa65c8
Govind Bavkar <tectignisofficial@gmail.com>
2022-11-09 12:08:08 +0530

    CHANGES

---
+++ returns-refund.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+<!-- belle/about-us.html   11 Nov 2019 12:44:33 GMT -->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<title>About Us &ndash; Belle Multipurpose Bootstrap 4 Template</title>
+<meta name="description" content="description">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Favicon -->
+<link rel="shortcut icon" href="assets/images/favicon.png" />
+<!-- Plugins CSS -->
+<link rel="stylesheet" href="assets/css/plugins.css">
+<!-- Bootstap CSS -->
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+<!-- Main Style CSS -->
+<link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+<body class="page-template belle">
+<div class="pageWrapper">
+	<!--Search Form Drawer-->
+	<div class="search">
+        <div class="search__form">
+            <form class="search-bar__form" action="#">
+                <button class="go-btn search__button" type="submit"><i class="icon anm anm-search-l"></i></button>
+                <input class="search__input" type="search" name="q" value="" placeholder="Search entire store..." aria-label="Search" autocomplete="off">
+            </form>
+            <button type="button" class="search-trigger close-btn"><i class="icon anm anm-times-l"></i></button>
+        </div>
+    </div>
+    <!--End Search Form Drawer-->
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+    
+    <!--Body Content-->
+    <div id="page-content">
+    	<!--Page Title-->
+    	<div class="page section-header text-center">
+			<div class="page-title">
+        		<div class="wrapper"><h1 class="page-width">Returns & Refunds</h1>
+				<div class="breadcrumbs"><a href="" >Home</a> » <span class="current">Returns & Refunds</span></div>
+				</div>
+      		</div>
+		</div>
+        <!--End Page Title-->
+        
+        <div class="container">
+        	<div class="row">
+                <div class="col-12 col-sm-12 col-md-12 col-lg-12 main-col">
+                    <div class="rte-setting">
+                            <p>We are happy to accept a return or exchange within 15 business days from the date of delivery of the concerned product only. The buyer will have to pay the return shipping charges. Once we receive the returned item, the return process is initiated from our end and the condition email of the returned item is sent out immediately & then the refund process is initiated between the next 3-4 days.</p>
+							<p>Buyer to inform of the same on receipt of damaged product by writing to us at info@z1kneebrace.com</p>
+							<li>Buyer to ship the product back at the notified address (Jodhpur, Rajasthan, India) at its own cost after the receipt of our e-mail consent;</li>
+							<li>On receipt of the damaged product by us, we will send you the exchanged product.</li>
+							<p>Please note that no exchange / return / refund will be entertained if a customer wants to do so for the reason that he/she doesn’t like the product after delivery or feels the product doesn’t match his or her expectations. No refunds will be given in the following cases:</p>
+							<li>Incorrect or insufficient address mentioned by the customer;</li>
+							<li>Non-availability of recipient at the mentioned address and/or premises;</li>
+							<li>Refusal to accept products;</li>
+							<li>Delivered at the place/to the person specifically mentioned by the customer other than the customer himself;</li>
+							<li>Force majeure event;</li>
+							<li>In case the product has undergone any tampering by the customer.</li>
+							<p>Please note that all the shipping charges and duties incidental to the return of product shall be borne by the buyer. Z1 Knee Brace has no liability towards it.</p>
+                    </div>
+               	</div>
+            </div>
+        </div>
+        
+    </div>
+    <!--End Body Content-->
+    
+    <!--Footer-->
+    
+	
+	
+	
+	
+	
+	
+    <!--End Footer-->
+    <!--Scoll Top-->
+    <span id="site-scroll"><i class="icon anm anm-angle-up-r"></i></span>
+    <!--End Scoll Top-->
+    
+     <!-- Including Jquery -->
+     <script src="assets/js/vendor/jquery-3.3.1.min.js"></script>
+     <script src="assets/js/vendor/jquery.cookie.js"></script>
+     <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
+     <script src="assets/js/vendor/wow.min.js"></script>
+     <!-- Including Javascript -->
+     <script src="assets/js/bootstrap.min.js"></script>
+     <script src="assets/js/plugins.js"></script>
+     <script src="assets/js/popper.min.js"></script>
+     <script src="assets/js/lazysizes.js"></script>
+     <script src="assets/js/main.js"></script>
+</div>
+</body>
+
+<!-- belle/about-us.html   11 Nov 2019 12:44:39 GMT -->
+</html>
---
+++ shipping-delivery.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+<!-- belle/about-us.html   11 Nov 2019 12:44:33 GMT -->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<title>About Us &ndash; Belle Multipurpose Bootstrap 4 Template</title>
+<meta name="description" content="description">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Favicon -->
+<link rel="shortcut icon" href="assets/images/favicon.png" />
+<!-- Plugins CSS -->
+<link rel="stylesheet" href="assets/css/plugins.css">
+<!-- Bootstap CSS -->
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+<!-- Main Style CSS -->
+<link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+<body class="page-template belle">
+<div class="pageWrapper">
+	<!--Search Form Drawer-->
+	<div class="search">
+        <div class="search__form">
+            <form class="search-bar__form" action="#">
+                <button class="go-btn search__button" type="submit"><i class="icon anm anm-search-l"></i></button>
+                <input class="search__input" type="search" name="q" value="" placeholder="Search entire store..." aria-label="Search" autocomplete="off">
+            </form>
+            <button type="button" class="search-trigger close-btn"><i class="icon anm anm-times-l"></i></button>
+        </div>
+    </div>
+    <!--End Search Form Drawer-->
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+    
+    <!--Body Content-->
+    <div id="page-content">
+    	<!--Page Title-->
+    	<div class="page section-header text-center">
+			<div class="page-title">
+        		<div class="wrapper"><h1 class="page-width">Shipping & Delivery</h1>
+					<div class="breadcrumbs"><a href="" >Home</a> » <span class="current">Shipping & Delivery</span></div>
+				</div>
+      		</div>
+		</div>
+        <!--End Page Title-->
+        
+        <div class="container">
+        	
+            <div class="row">
+            	<div class="col-12">
+					<p>We provide free shipping for all our orders for the time being.</p>
+                    <h2>Delivery Destinations:</h2>
+                    <p>We deliver all over India and Abroad!</p>
+                    <p>If you happen to face any problems while checking out please WhatsApp us on 7300044197 write to us at info@z1kneebrace.com . The lead time before we dispatch the product is 24-48 business hours wherein the buyer can confirm the sizes through mail.</p>
+                </div>
+            </div>
+            
+        <div class="row">
+            	<div class="col-12">
+					<p><strong></strong></p>
+                    <h2>How do we deliver?</h2>
+                    <p>Shipping for addresses in the US is done by FedEx/DHL. While all the other shipping such as APO/FPO, PO Box including USA APO/FPO, PO Box etc. are done by INDIA POST SPEED POST SERVICE.</p>
+					<p><strong>International Deliveries:</strong> Where the International deliveries are made via FedEx/DHL, it may take up to 5-8 business days from the date of dispatch however the deliveries made through national posts of the respective countries such as APO/FPO, PO Box, it may take up to 10-15 business days from the date of dispatch.</p> 
+					<p>National Deliveries: We deliver across India through most trusted and fast courier services. Deliveries made through FedEx and Blue Dart mat take up to 5-8 business days from the date of dispatch.</p> 
+					<p>While placing your order please make sure to write any fine details you’d like mentioned in our notes section provided on the website. That ways we will make sure we follow exactly how you have directed us to make your order an easy and smooth process!</p> 
+                </div>
+            </div>
+			
+			<div class="row">
+            	<div class="col-12">
+					<p><strong></strong></p>
+                    <h2>Order Tracking:</h2>
+                    <p>Once your order has been dispatched from our delivery point, an email or an SMS will be sent to you on the email address and mobile number provided by you under “Billing Address” section while placing the Order. You can track your order online by the Tracking ID number sent to you by us on your Mobile/Email ID.<br>You may also log on to your account on the website or use the “Track my Order” link on the website to track your order.</p> 
+                </div>
+            </div>
+			
+			<div class="row">
+            	<div class="col-12">
+					<p><strong></strong></p>
+                    <h2>Order-Splitting:</h2>
+                    <p>Sometimes we may not be in a position to deliver an order to a particular location due to the nature of the product, its availability or due to the service limitations of our courier and logistic partners. In such cases, you may receive the items (of a single order) at different points of time or through different courier partners. The status of all items within an order can be tracked online.</p>
+					<p><strong>Please note that the custom and other duties leviable on the purchase of product are a liability of the buyer and in no case will be borne by Z1 Knee Brace.</strong></p>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+    <!--End Body Content-->
+    
+    <!--Footer-->
+    
+	
+	
+	
+	
+	
+	
+    <!--End Footer-->
+    <!--Scoll Top-->
+    <span id="site-scroll"><i class="icon anm anm-angle-up-r"></i></span>
+    <!--End Scoll Top-->
+    
+     <!-- Including Jquery -->
+     <script src="assets/js/vendor/jquery-3.3.1.min.js"></script>
+     <script src="assets/js/vendor/jquery.cookie.js"></script>
+     <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
+     <script src="assets/js/vendor/wow.min.js"></script>
+     <!-- Including Javascript -->
+     <script src="assets/js/bootstrap.min.js"></script>
+     <script src="assets/js/plugins.js"></script>
+     <script src="assets/js/popper.min.js"></script>
+     <script src="assets/js/lazysizes.js"></script>
+     <script src="assets/js/main.js"></script>
+</div>
+</body>
+
+<!-- belle/about-us.html   11 Nov 2019 12:44:39 GMT -->
+</html>
