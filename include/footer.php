181e89e6649063a672ed8d999086a7720f2f4626
monikagore <mobnikgore5=6@m=gmail.com>
2022-11-11 16:44:55 +0530

    social media links

--- include/footer.php
+++ include/footer.php
@@ -13,11 +13,7 @@ if(isset($_POST['submit'])){
 }
 ?>
 
- <?php                         
-$sql=mysqli_query($conn,"select facebook from social_media");
-$facebook=mysqli_fetch_array($sql);
 
-?>
  <!--Footer-->
  <footer id="footer">
      <div class="newsletter-section">
@@ -46,30 +42,35 @@ $facebook=mysqli_fetch_array($sql);
                  <div class="col-12 col-sm-12 col-md-12 col-lg-5 d-flex justify-content-end align-items-center">
                      <div class="footer-social">
                          <ul class="list--inline site-footer__social-icons social-icons">
+                         <?php                         
+$sql=mysqli_query($conn,"select * from social_media");
+$facebook=mysqli_fetch_array($sql);
+
+?>
                              <li><a class="social-icons__link" href="<?php echo $facebook['facebook']?>" target="_blank"
                                      title="Belle Multipurpose Bootstrap 4 Template on Facebook"><i
                                          class="icon icon-facebook"></i></a></li>
-                             <li><a class="social-icons__link" href="#" target="_blank"
+                             <li><a class="social-icons__link" href="<?php echo $facebook['twitter']?>" target="_blank"
                                      title="Belle Multipurpose Bootstrap 4 Template on Twitter"><i
                                          class="icon icon-twitter"></i> <span
                                          class="icon__fallback-text">Twitter</span></a></li>
-                             <li><a class="social-icons__link" href="#" target="_blank"
+                             <li><a class="social-icons__link" href="<?php echo $facebook['pinterest']?>" target="_blank"
                                      title="Belle Multipurpose Bootstrap 4 Template on Pinterest"><i
                                          class="icon icon-pinterest"></i> <span
                                          class="icon__fallback-text">Pinterest</span></a></li>
-                             <li><a class="social-icons__link" href="#" target="_blank"
+                             <li><a class="social-icons__link" href="<?php echo $facebook['instagram']?>" target="_blank"
                                      title="Belle Multipurpose Bootstrap 4 Template on Instagram"><i
                                          class="icon icon-instagram"></i> <span
                                          class="icon__fallback-text">Instagram</span></a></li>
-                             <li><a class="social-icons__link" href="#" target="_blank"
+                             <li><a class="social-icons__link" href="<?php echo $facebook['tumblr']?>" target="_blank"
                                      title="Belle Multipurpose Bootstrap 4 Template on Tumblr"><i
                                          class="icon icon-tumblr-alt"></i> <span
                                          class="icon__fallback-text">Tumblr</span></a></li>
-                             <li><a class="social-icons__link" href="#" target="_blank"
+                             <li><a class="social-icons__link" href="<?php echo $facebook['youtube']?>" target="_blank"
                                      title="Belle Multipurpose Bootstrap 4 Template on YouTube"><i
                                          class="icon icon-youtube"></i> <span
                                          class="icon__fallback-text">YouTube</span></a></li>
-                             <li><a class="social-icons__link" href="#" target="_blank"
+                             <li><a class="social-icons__link" href="<?php echo $facebook['vimeo']?>" target="_blank"
                                      title="Belle Multipurpose Bootstrap 4 Template on Vimeo"><i
                                          class="icon icon-vimeo-alt"></i> <span
                                          class="icon__fallback-text">Vimeo</span></a></li>
