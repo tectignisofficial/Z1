1641068c66c80b74eecdfc2c93cf30c528efa07d
maheshniwate <maheshniwate10@gmail.com>
2022-11-12 13:31:25 +0530

    3 done

--- auth/code/sadmin/blogtable.php
+++ auth/code/sadmin/blogtable.php
@@ -28,7 +28,7 @@ include("../../../include/config.php");
     <link rel="shortcut icon" type="image/x-icon" href="app-assets/images/ico/favicon.ico" />
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600"
         rel="stylesheet" />
-
+ 
     <!-- DataTables -->
     <link rel="stylesheet" href="plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
     <link rel="stylesheet" href="plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
