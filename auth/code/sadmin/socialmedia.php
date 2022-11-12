a2b4e21699e8bb9066fc670e560971de05049eaa
monikagore <mobnikgore5=6@m=gmail.com>
2022-11-12 11:06:43 +0530

    ch

--- auth/code/sadmin/socialmedia.php
+++ auth/code/sadmin/socialmedia.php
@@ -238,6 +238,7 @@ if(isset($_POST['update'])){
         <div class="content-wrapper container-xxl p-0">
             <div class="content-header row">
                 <div class="content-header-left col-md-9 col-12 mb-2">
+                <h2 class="content-header-title float-start mb-0">Social Media</h2>
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="index.php">Dashboard</a>
                         </li>
