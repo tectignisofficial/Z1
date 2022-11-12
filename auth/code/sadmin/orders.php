0e9d9470dcca0fb4643be3c040a84455161cd687
maheshniwate <maheshniwate10@gmail.com>
2022-11-12 18:01:32 +0530

    done

--- auth/code/sadmin/orders.php
+++ auth/code/sadmin/orders.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html class="loading" lang="en" data-textdirection="ltr">
 <!-- BEGIN: Head-->
-
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
