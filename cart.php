0dde85341f53dffde51b6e4602de1e39396bf541
keshridivya <dkeshari094@gmail.com>
2022-11-16 10:37:37 +0530

    c

--- cart.php
+++ cart.php
@@ -9,6 +9,7 @@ if(isset($_GET['did'])){
         }
         unset($_SESSION['shopping_cart']);
     }
+    header('location:cart.php');
 }
 
 if(isset($_POST['checkout'])){
@@ -51,6 +52,49 @@ if(isset($_POST['checkout'])){
 <!-- Main Style CSS -->
 <link rel="stylesheet" href="assets/css/style.css">
 <link rel="stylesheet" href="assets/css/responsive.css">
+<style>
+    .mt-100{
+   margin-top:100px;
+
+}
+
+
+.card1 {
+    margin-bottom: 30px;
+    border: 0;
+    -webkit-transition: all .3s ease;
+    transition: all .3s ease;
+    letter-spacing: .5px;
+    border-radius: 8px;
+    -webkit-box-shadow: 1px 5px 24px 0 rgba(68,102,242,.05);
+    box-shadow: 1px 5px 24px 0 rgba(68,102,242,.05);
+}
+
+.card1 .card-header1 {
+    background-color: #fff;
+    border-bottom: none;
+    padding: 24px;
+    border-bottom: 1px solid #f6f7fb;
+    border-top-left-radius: 8px;
+    border-top-right-radius: 8px;
+}
+
+.card-header1:first-child {
+    border-radius: calc(.25rem - 1px) calc(.25rem - 1px) 0 0;
+}
+
+
+
+.card1 .card-body1 {
+    padding: 30px;
+    background-color: transparent;
+}
+
+.btn-primary, .btn-primary.disabled, .btn-primary:disabled {
+    background-color: #4466f2!important;
+    border-color: #4466f2!important;
+}
+</style>
 </head>
 <body class="page-template belle">
 <div class="pageWrapper">
@@ -215,7 +259,33 @@ if(isset($_POST['checkout'])){
             </div>
             <?php
               }else{
-                echo '<tr><td>cart empty</td></tr>';
+                echo '<div class="container-fluid  mt-100">
+                <div class="row">
+                
+                   <div class="col-md-12">
+                   
+                           <div class="card1">
+                       <div class="card-header1">
+                       
+                       </div>
+                       <div class="card-body1 cart">
+                               <div class="col-sm-12 empty-cart-cls text-center">
+                                   <img src="https://i.imgur.com/dCdflKN.png" width="130" height="130" class="img-fluid mb-4 mr-3">
+                                   <h3><strong>Your Cart is Empty</strong></h3>
+                                   <h4>Add something to make me happy :)</h4>
+                                   <a href="index.php" class="btn btn-primary cart-btn-transform m-3" data-abc="true">continue shopping</a>
+                                   
+                               
+                               </div>
+                       </div>
+               </div>
+                       
+                   
+                   </div>
+                
+                </div>
+               
+               </div>';
             } 
             ?>
         </div>
